4d66ab8dfde172ff26b796d8f29c0a45d136d1aa
CepheusSun <cd_sunyang@163.com>
2017-04-17 20:14:07 +0800

    first commit & gitignore

---
+++ fake-netease-music/fake-netease-music/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fake-netease-music
+//
+//  Created by sunny on 2017/4/17.
+//  Copyright © 2017年 CepheusSun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
