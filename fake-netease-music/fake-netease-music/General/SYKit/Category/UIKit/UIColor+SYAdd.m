8256727ca603aab47f49f91893cf2a386a3214d5
CepheusSun <cd_sunyang@163.com>
2017-04-18 13:05:52 +0800

    NSObject category method swizzling

--- fake-netease-music/fake-netease-music/General/SYKit/Category/UIKit/UIColor+SYAdd.m
+++ fake-netease-music/fake-netease-music/General/SYKit/Category/UIKit/UIColor+SYAdd.m
@@ -87,4 +87,28 @@
     
     return UIColorFromRGBBytes(r, g, b);
 }
+
+#pragma mark - 渐变颜色
++ (UIColor *)gradientFrom:(UIColor *)fromColor
+                       to:(UIColor *)toColor
+                   height:(CGFloat)height
+{
+    CGSize size = CGSizeMake(1, height);
+    UIGraphicsBeginImageContextWithOptions(size, NO, 1.0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGColorSpaceRef space = CGColorSpaceCreateDeviceRGB();
+    
+    NSArray* colors = [NSArray arrayWithObjects:(id)fromColor.CGColor, (id)toColor.CGColor, nil];
+    CGGradientRef gradient = CGGradientCreateWithColors(space, (__bridge CFArrayRef)colors, NULL);
+    CGContextDrawLinearGradient(context, gradient, CGPointMake(0, 0), CGPointMake(0, size.height), 0);
+    
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    
+    CGGradientRelease(gradient);
+    CGColorSpaceRelease(space);
+    UIGraphicsEndImageContext();
+    
+    return [UIColor colorWithPatternImage:image];
+}
 @end
