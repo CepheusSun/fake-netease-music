e0dff720e202d91141efd60ab98755a04881c842
CepheusSun <cd_sunyang@163.com>
2017-04-18 15:33:33 +0800

    UIImage

--- fake-netease-music/fake-netease-music/General/SYKit/Category/UIKit/UIImage+SYAdd.h
+++ fake-netease-music/fake-netease-music/General/SYKit/Category/UIKit/UIImage+SYAdd.h
@@ -16,7 +16,12 @@ UIKIT_EXTERN void saveVideoAtURLToSavedPhotosAlbum(NSURL *videoURL, void(^comple
 
 @interface UIImage (SYAdd)
 
-
+//尺寸压缩，质量不变
+- (UIImage *)scaleToSize:(CGSize)size;
+//质量压缩，尺寸不变，可能压缩不到你要的大小，因为没办法到那么小 这时候你就需要调用尺寸压缩了
+- (NSData *)dataInNoSacleLimitBytes:(NSInteger)bytes;
+//裁剪图片 取中上部分
+- (UIImage *)cutToSize:(CGSize)size;
 
 @end
 NS_ASSUME_NONNULL_END
